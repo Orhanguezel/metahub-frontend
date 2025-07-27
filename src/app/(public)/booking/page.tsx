9deddac13a1f447ca90a5e63346b67dc20d9ba72
Orhan Güzel <orhan.guezel@dci-student.org>
2025-07-27 06:01:06 +0200

    fehler

--- src/app/(public)/booking/page.tsx
+++ src/app/(public)/booking/page.tsx
@@ -1,7 +1,14 @@
+// src/app/(public)/booking/page.tsx
+
 "use client";
-import React from "react";
+import React, { Suspense } from "react";
 import { BookingPublicPage } from "@/modules/booking";
 
+// Suspense boundary ile sarmala!
 export default function BookingRouterPage() {
-  return <BookingPublicPage />;
+  return (
+    <Suspense fallback={null}>
+      <BookingPublicPage />
+    </Suspense>
+  );
 }
