43bfa9edacf18e5734ef9687ed3e1f5a35c549b0
Orhan Güzel <orhan.guezel@dci-student.org>
2025-08-27 19:17:22 +0200

    kleine fechler

--- src/app/(public)/references/[slug]/page.tsx
+++ src/app/(public)/references/[slug]/page.tsx
@@ -1,21 +1,14 @@
 import Page from "@/modules/references/public/pages/Page";
 
-// Next 15+: searchParams Promise
-type RouteProps = {
-  params: { slug?: string[] };
+export default async function ReferencesRoutePage({
+  params,
+  searchParams,
+}: {
+  params: Promise<{ slug: string }>;
   searchParams: Promise<{ p?: string; ps?: string }>;
-};
-
-export default async function ReferencesRoutePage({ params, searchParams }: RouteProps) {
+}) {
+  const { slug } = await params;
   const sp = await searchParams;
 
-  // /references => undefined, /references/<slug> => params.slug[0]
-  const categorySlug = Array.isArray(params.slug) ? params.slug[0] : undefined;
-
-  return (
-    <Page
-      categorySlug={categorySlug}
-      initialSearch={sp}
-    />
-  );
+  return <Page categorySlug={slug} initialSearch={sp} />;
 }
